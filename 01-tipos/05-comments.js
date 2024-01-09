ab1686c5859879eafa7b68cf726481794e8d620e
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-08 22:11:26 -0300

    In line and multiline comments

---
+++ 01-tipos/05-comments.js
@@ -0,0 +1,14 @@
+// this is a comment
+let number = 42; // this is another comment
+/**
+ * this is 
+ * a multiline
+ * comment
+*/
+
+/*
+this is another way 
+to write a multiline 
+comment
+*/
+console.log(number); 
