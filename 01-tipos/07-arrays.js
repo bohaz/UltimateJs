3ccce3434391cb253915efebaeaafa4bcbad7062
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-08 22:35:43 -0300

    Arrays

---
+++ 01-tipos/07-arrays.js
@@ -0,0 +1,10 @@
+let animals = ["perro", "caballo"];
+console.log(animals);
+console.log(animals[0]);
+animals[2] = "gato";
+console.log(animals);
+
+animals[10] = "elefante";
+console.log(animals[7]);
+console.log(typeof animals);
+console.log(animals.length);
