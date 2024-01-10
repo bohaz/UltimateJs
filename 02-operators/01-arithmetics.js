67736b173e6579e19ffcfe208d542cb2fe5de43e
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-10 13:20:14 -0300

    Rename the file

---
+++ 02-operators/01-arithmetics.js
@@ -0,0 +1,18 @@
+let a = 5;
+let b = 7;
+
+console.log (a + b); // sum
+console.log (a - b); // difference
+console.log (a * b); // product
+console.log (a / b); // quotient
+console.log (a % b); // remainder
+console.log (a ** b); // exponentiation
+
+// Increment and decrement operators
+
+let c = 5;
+console.log(++c);
+console.log(c++);
+
+console.log(--c);
+console.log(c--);
