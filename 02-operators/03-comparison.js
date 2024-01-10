d625cb62e7232fccfa13de30b9d95ca10200220e
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-10 13:31:15 -0300

    Refreshing comparison operators

---
+++ 02-operators/03-comparison.js
@@ -0,0 +1,11 @@
+let a = 10;
+console.log(a > 5 );
+console.log(a >= 5); 
+console.log(a < 5);
+console.log(a <= 5);
+
+console.log(a == 10);
+console.log(a != 5);
+console.log(a == "10");
+console.log(a === "10"); // strict equality
+console.log(a !== "10");
