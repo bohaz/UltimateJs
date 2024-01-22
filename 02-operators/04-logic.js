5d0c35a1a7f28b64efe6ca76668dd16fc5f077ca
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-22 17:07:04 -0300

    Logic operators

---
+++ 02-operators/04-logic.js
@@ -0,0 +1,16 @@
+// AND, OR, NOT
+
+// AND (&&)
+
+let mayor = true;
+let suscrito = true;
+//console.log(true && true); // true
+//console.log(true && false); // false
+console.log("operador AND ", mayor && suscrito); // true
+
+// OR (||)
+console.log("operador OR ", mayor || suscrito); // true
+
+// NOT (!)
+console.log("operador NOT ", !mayor); // false
+let catalogoInfantil = !mayor;
