3581920be8dd74d51801f68eccdda7641c4fcfad
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-25 11:37:29 -0300

    Refreshing infinite loops

---
+++ 03-flow-control/04-infinite-loop.js
@@ -0,0 +1,7 @@
+let i = 0;
+while (i < 10) {
+ console.log(i);
+  i++; // This is the incrementer. It is very important to have this line, otherwise the loop will run forever.
+}
+
+console.log('done');
