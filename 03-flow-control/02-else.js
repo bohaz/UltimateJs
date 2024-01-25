412700532193379c60ff0fbe3e879a3fda7e228d
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-25 11:22:17 -0300

    refreshing else and else if statement

---
+++ 03-flow-control/02-else.js
@@ -0,0 +1,10 @@
+let age = 18;
+
+if (age > 18) {
+  console.log('user is older than 18');
+} else if (age === 18) {
+  console.log('user is 18');
+}
+else {
+  console.log('user is younger than 18');
+}
