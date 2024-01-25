409de3e2a7c1ad80d7b136c372e5e133d1c5b4b8
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-25 11:53:44 -0300

    Refreshing for loops

---
+++ 03-flow-control/06-for.js
@@ -0,0 +1,15 @@
+/* let i = 2;
+while (i < 2) {
+  if(i % 2 === 0) {
+    console.log('Even Number ',i);
+  }
+  i++;
+} */
+
+//for(initializer; condition; incrementer) {}
+
+for(let i = 2; i < 10; i++) {
+  if(i % 2 === 0) {
+    console.log('Even Number ',i);
+  }
+}
