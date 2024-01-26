fa0ec5ea604c7fb39a0778ee5e243f0522122c17
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-26 17:20:12 -0300

    Exercise number 1

---
+++ 04-exercises/01.js
@@ -0,0 +1,22 @@
+/* function whichIsBigger(a, b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+}
+
+let bigger = whichIsBigger(10, 5);
+
+console.log(bigger);
+ */
+
+// ternary operator
+
+function whichIsBigger(a, b) {
+  return a > b ? a : b;
+} 
+
+let bigger = whichIsBigger(10, 5);
+
+console.log(bigger);
