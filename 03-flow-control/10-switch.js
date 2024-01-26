972bd64548dc033514c69177f111d866a5c2edf3
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-26 17:02:07 -0300

    Refreshing switch

---
+++ 03-flow-control/10-switch.js
@@ -0,0 +1,13 @@
+let action = 'sleep';
+
+switch (action) {
+  case 'eat':
+    console.log('The action is eat');
+    break;
+  case 'sleep':
+    console.log('The action is sleep');
+    break;
+  default:
+    console.log('The action is unknown');
+    break;
+}
