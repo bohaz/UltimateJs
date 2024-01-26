cbff58d957d9a58138142b6d3b8421e9f1c5e9f5
Ricardo Martínez <ricardomartinezvet@gmail.com>
2024-01-26 16:40:49 -0300

    Knowing for of

---
+++ 03-flow-control/07-for-of.js
@@ -0,0 +1,15 @@
+// while
+// do while
+// for
+
+let animals = ['Donkey', 'Horse', 'Dog'];
+
+for (let animal of animals) {
+  console.log(animal);
+}
+
+let i = 0;
+while (i < animals.length) {
+  console.log(animals[i]);
+  i++;
+}
